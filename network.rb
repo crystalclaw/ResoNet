7124c9f78aa950b47b8cf813abce2258dbb16ef3
George Czarnecki <gdc249@gmail.com>
2015-02-14 17:53:36 -0600

    network.rb
    Attempt to Send Packets

--- network.rb
+++ network.rb
@@ -2,16 +2,15 @@
 create class 'Network'
 1969  contains- fucntions for reception and sending of packets, whole network, timestamp included.
 =end
+require 'socket'                # Get sockets from stdlib
 
-require 'socket'
-
-hostname = 'localhost'
-port = 1969
-
-open_socket = TCPSocket.open(hostname, port)
-
-while line = open_socket.gets
-  puts line.chop
-end
-
+server = TCPServer.open(1969)   # Socket to listen on port 2000
+loop {                          # Servers run forever
+  Thread.start(server.accept) do |client|
+    client.puts(Time.now.strftime("%Y-%m-%d %H:%M:%S.%L")) # Send the time to the client
+    client.puts(request_packet)
+    client.puts "Packet_Sent"
+    client.close                
+  end
+}
 open_socket.close
