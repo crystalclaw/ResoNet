134ea18a1b85016a8f594fd0334502d2968a088c
George Czarnecki <gdc249@gmail.com>
2015-02-15 00:38:33 -0600

    Update network.rb

--- network.rb
+++ network.rb
@@ -1,17 +1,17 @@
 port1 = 256
 port2 = 255
-quit1 = false
+connection_closed = false
 require 'socket'
 servant = TCPServer.new('localhost', port1)
 servoid = TCPServer.new('localhost', port2)
 loop do
-  until connection.close
+  until connection_closed == true
     connection = servoid.accept
     if connection.start(servoid.accept) then
       puts 'received connection'
       connection.write 'Data Caught'
     else
-      connection.close
+      connection.close + connection_closed = true
     end
     servant.write 'Data Pitched'
     puts 'Data Got: ' + servant.recv(port2)
