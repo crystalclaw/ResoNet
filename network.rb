07d7d3fe75f13e5571f33c849d60f93c390009ed
George Czarnecki <gdc249@gmail.com>
2015-02-14 20:17:55 -0600

    Update network.rb

--- network.rb
+++ network.rb
@@ -4,14 +4,17 @@ create class 'Network'
 =end
 port1= 256
 port2= 255
+quit = false
 require 'socket'
 servant = TCPSocket.new('localhost', port1)
 servoid = TCPServer.new('localhost', port2)
 loop{
-  connection = servoid.accept
-  puts 'received: ' + connection.recv(port1)
-  connection.write 'Data Caught'
-  connection.close
+  until quit = true
+    connection = servoid.accept
+    puts 'received: ' + connection.recv(port1)
+    connection.write 'Data Caught'
+    connection.close
+  end
 
   servant.write 'Data Pitched'
   puts 'Data Got: ' + servant.recv(port2)
