2f437554d7bd8a2e7bcc431739280a6655e7fbca
George Czarnecki <gdc249@gmail.com>
2015-02-15 03:30:35 -0600

    Update network.rb

--- network.rb
+++ network.rb
@@ -1,18 +1,43 @@
-port1 = 256
-port2 = 255
-connection_closed = false
-require 'socket'
-servant = TCPServer.new('localhost', port1)
-servoid = TCPServer.new('localhost', port2)
-puts 'Running Program'
-loop do
-  until connection_closed if
-    connection then
-    connection.write 'received connection'
-    connection.close
-    connection_closed = true
+require 'socket'  # TCPServer
+ss = TCPServer.new(1233)
+loop {
+  Thread.start(ss.accept) { |s|
+    begin
+      while line = s.gets;  # Returns nil on EOF.
+        (s << "You wrote: #{line.inspect}\r\n").flush
+      end
+    rescue
+      bt = $!.backtrace * "\n  "
+      ($stderr << "error: #{$!.inspect}\n  #{bt}\n").flush
+    ensure
+      s.close
     end
-  servant.write 'Data Pitched'
-  puts 'Data Got: ' + servant.recv(port2)
+  }
+}
+require 'thread'  # Queue
+ssock = TCPServer.new(1234)
+msgs = Queue.new
+participants = []
+Thread.start {  # Send chat messages to participants.
+  while msg = msgs.pop;  # Always true.
+    participants.each { |s|
+      (s << msg).flush rescue IOError
+    }
   end
-end
+}
+loop {
+  Thread.start(ssock.accept) { |sock|
+    participants << sock
+    begin
+      while line = sock.gets;  # Returns nil on EOF.
+        msgs << ": #{line.chomp!}\r\n"
+      end
+    rescue
+      bt = $!.backtrace * "\n  "
+      ($stderr << "error: #{$!.inspect}\n  #{bt}\n").flush
+    ensure
+      participants.delete sock
+      sock.close
+    end
+  }
+}
