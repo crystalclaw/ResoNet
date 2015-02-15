34c541b20477d134d84915d31b40e366e22fc24b
George Czarnecki <gdc249@gmail.com>
2015-02-15 01:57:23 -0600

    Update network.rb

--- network.rb
+++ network.rb
@@ -8,7 +8,7 @@ loop do
   until connection_closed == true
     connection = servoid.accept
     if connection.start(servoid.accept) then
-      puts 'received connection'
+      print 'received connection'
       connection.write 'Data Caught'
     else
       connection.close + connection_closed = true
@@ -17,21 +17,3 @@ loop do
     puts 'Data Got: ' + servant.recv(port2)
   end
 end
-=begin
-require 'socket'  # TCPServer
-ss = TCPServer.new(1233)
-loop {
-  Thread.start(ss.accept) { |s|
-    begin
-      while line = s.gets;  # Returns nil on EOF.
-        (s << "You wrote: #{line.inspect}\r\n").flush
-      end
-    rescue
-      bt = $!.backtrace * "\n  "
-      ($stderr << "error: #{$!.inspect}\n  #{bt}\n").flush
-    ensure
-      s.close
-    end
-  }
-}
-=end
