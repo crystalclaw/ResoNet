f9747440e07f626c01d36390198964e7e5e5aaad
Crystalclaw <crystalclaw1@gmail.com>
2015-02-14 19:40:11 -0600

    Updated audio.py

--- audio.py
+++ audio.py
@@ -1,4 +1,10 @@
 import pyaudio
 pyaud = pyaudio.Pyaudio()
-
-def callback(
+audioBuffer = {}
+def callback(in_data, frame_count, time_info, status_flags):
+    #get closest timestamp here
+    timestamp = 0
+    out_data = audioBuffer[timestamp]
+    return (out_data, flag)
+def add_audio_data(frames, timestamp):
+    audioBuffer[timestamp] = frames
