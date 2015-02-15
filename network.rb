8ca1ae150824b41175d87aac7ae43bdba3c846f5
George Czarnecki <gdc249@gmail.com>
2015-02-14 18:26:28 -0600

    Network.rb
    Made it loop between two functions, without looping within.

--- network.rb
+++ network.rb
@@ -2,17 +2,17 @@
 create class 'Network'
 1969  contains- fucntions for reception and sending of packets, whole network, timestamp included.
 =end
+loop{
 require 'socket'
 servoid = TCPServer.new('', 1969)
-loop {
   connection = servoid.accept
   puts 'received: ' + connection.recv(9691)
   connection.write 'Data Caught'
   connection.close
-}
 
 require 'socket'
 servant = TCPSocket.new('', 9691)
 servant.write 'Data Pitched'
 puts 'Data Got: ' + servant.recv(1969)
 servant.close
+}
